dbac12169b4f3e5227cae3aa656a2f4ee1471ce3
adamgutt <93285321+adamgutt@users.noreply.github.com>
2022-05-18 14:53:46 -0400

    added proptypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -11,11 +11,19 @@ export class MovieCard extends React.Component {
   }
 }
 
+//setting up default values for the MovieCard properties
+//ensuring values are strings and required
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+
+  onMovieClick: PropTypes.func.isRequired,
 };
