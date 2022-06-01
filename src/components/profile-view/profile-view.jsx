86c86d59ffca598f6afc9fb186fbf2b71c00a5b7
adamgutt <93285321+adamgutt@users.noreply.github.com>
2022-06-01 15:34:48 -0400

    test

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -9,33 +9,69 @@ import UpdateUser from './update-user';
 
 
 
+export function ProfileView(props) {
+  const [user, setUser] = useState(props.user);
+  const [movies, setMovies] = useState(props.movies);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const currentUser = localStorage.getItem('user');
+  const token = localStorage.getItem('token');
 
+  const getUser = () => {
+    axios.get(`https://movie-api-93167.herokuapp.com/users/${currentUser}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        setUser(response.data);
+        setFavoriteMovies(response.data.FavoriteMovies)
+      })
+      .catch(error => console.error(error))
+  }
 
+  useEffect(() => {
+    getUser();
+  }, [])
 
+  const handleDelete = () => {
+    axios.delete(`https://movie-api-93167.herokuapp.com/users/${currentUser}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(() => {
+        alert(`The account ${user.Username} was successfully deleted.`)
+        localStorage.clear();
+        window.open('/register', '_self');
+      }).
+      catch(error => console.error(error))
+  }
 
-
-return (
-  <Container>
-    <Row>
-      <Col xs={12} sm={4}>
-        <Card>
-          <Card.Body>
-            <UserInfo name={user.Username} email={user.Email} />
-          </Card.Body>
-        </Card   
-        </Col>
-
-      <Col xs={12} sm={8}>
-        <Card>
-          <Card.Body>
-            <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-
-    <FavoriteMovies favoriteMovieList={favoriteMovieList} />
-
-  </Container>
-);
-}  
+  return (
+    <Container id="profile-form">
+      <Row><h4>Your profile</h4></Row>
+      <Row>
+        <Col className="label">Username:</Col>
+        <Col className="value">{user.Username}</Col>
+      </Row>
+      <Row className="mt-3">
+        <Col className="label">Password:</Col>
+        <Col className="value">******</Col>
+      </Row>
+      <Row className="mt-3">
+        <Col className="label">Email:</Col>
+        <Col className="value">{user.Email}</Col>
+      </Row>
+      <Row className="mt-3">
+        <Col className="label">Birthday:</Col>
+        <Col className="value">{user.Birthday}</Col>
+      </Row>
+      <Row className="mt-5"><h4>Your favorite movies</h4></Row>
+      <Row className="mt-3">
+        <FavoriteMoviesView
+          movies={movies}
+          favoriteMovies={favoriteMovies}
+          currentUser={currentUser}
+          token={token} />
+      </Row>
+      <UpdateView user={user} />
+      <Button className="d-block mt-5" variant="warning" onClick={handleDelete}>Delete profile</Button>
+    </Container>
+  )
+}
