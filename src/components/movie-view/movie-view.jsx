f86954b2c0ed27d20afe4bc47afe3781ef9f5539
adamgutt <93285321+adamgutt@users.noreply.github.com>
2022-06-08 13:59:20 -0400

    p

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,33 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Col, Container, Row, Button } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+
+import { Button, Col, Container, Row } from 'react-bootstrap/';
+
+import './movie-view.scss';
 
 export class MovieView extends React.Component {
+  keypressCallback(event) {
+    console.log(event.key);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keypress', this.keypressCallback);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.keypressCallback);
+  }
 
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
-      <Container>
-        <Row>
-          <Col>
-            <Card id="movie-view">
-              <Card.Body>
-                <Card.Img id="movie-view-image" variant="top" src={movie.ImagePath} />
-                <Card.Title id="movie-title" className="movie-title">{movie.Title}</Card.Title>
-                <Card.Text id="movie-description" className="movie-description">
-                  {movie.Description}</Card.Text>
-                <Card.Text id="movie-director" className="movie-director">
-                  Director: {movie.Director.Name}</Card.Text>
-                <Card.Text id="movie-genre" className="movie-gerne">
-                  Genre: {movie.Genre.Name}</Card.Text>
-
-              </Card.Body>
-            </Card>
-            <Button id="movie-view-button" onClick={() => { onBackClick(null); }}>Back</Button>
-            <Button id="movie-view-button" onClick={() => { }}>Add to favorites</Button>
-          </Col>
+      <Container className="movie-view">
+        <Row className="movie-poster">
+          <img src={movie.ImagePath} />
         </Row>
+        <Row className="movie-title mt-3">
+          <Col className="label">Title: </Col>
+          <Col className="value">{movie.Title}</Col>
+        </Row>
+        <Row className="movie-description mt-3">
+          <Col className="label">Description: </Col>
+          <Col className="value">{movie.Description}</Col>
+        </Row>
+        <Link to={`/directors/${movie.Director.Name}`}>
+          <Button className="d-block mt-3" variant="info">Director</Button>
+        </Link>
+        <Link to={`/genres/${movie.Genre.Name}`}>
+          <Button className="d-block mt-3" variant="info">Genre</Button>
+        </Link>
+        <Button className="d-block mt-3" onClick={() => { onBackClick(null); }} variant="warning">Back</Button>
       </Container>
     );
   }
@@ -37,14 +51,16 @@ MovieView.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
-    }),
+    ImagePath: PropTypes.string.isRequired,
     Director: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+      Death: PropTypes.string
     }),
-    ImagePath: PropTypes.string.isRequired
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    })
   }).isRequired,
 };
