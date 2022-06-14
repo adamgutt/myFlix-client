8e2c6cfc6f94dcdf8e272dbc9f2fffd61ce69aeb
adamgutt <93285321+adamgutt@users.noreply.github.com>
2022-06-14 13:53:31 -0400

    updated redux version and fixed moviecard issue in mainview

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,7 +11,7 @@ import { setMovies, setUser } from '../../actions/actions';
 import MoviesList from '../movies-list/movies-list';
 
 import { LoginView } from '../login-view/login-view';
-// import { MovieCard } from '../movie-card/movie-card';
+import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { DirectorView } from '../director-view/director-view';
 import { GenreView } from '../genre-view/genre-view';
@@ -75,8 +75,7 @@ class MainView extends React.Component {
   }
 
   render() {
-    let { movies } = this.props;
-    let { user } = this.state;
+    let { user, movies } = this.state;
 
     return (
       <Router>
